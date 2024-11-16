0809571e36431cf680b023af6d8cab5cc203f9e2
qwertpi <qwertpi@users.noreply.github.com>
2024-11-16 19:59:27 +0000

    Decision tree hyperparameter tuning

--- models/decision_tree/xgb.py
+++ models/decision_tree/xgb.py
@@ -2,7 +2,6 @@ import sys
 
 sys.path.insert(0, "..")
 
-import click
 import numpy as np
 from sklearn.pipeline import make_pipeline  # type: ignore
 from xgboost import XGBClassifier, XGBRFClassifier
@@ -17,29 +16,27 @@ NUM_EXAMPLES = 646152
 trainer = PatchModelTrainer()
 
 
-@click.command()
 def train_model():
     for model in (XGBClassifier, XGBRFClassifier):
         for receptor in ("ER", "PR"):
             class_imbalance = (NUM_EXAMPLES - NUM_POS[receptor]) / NUM_POS[receptor]
-            """
-            Hyperparams:
-            n_estimators - number of trees in forest
-            max_depth
-            max_leaves
-            learning rate - eta of boosting
-            gamma - minimum loss reduction required to split a leaf
-            reg_alpha - amount of L1 regularisation
-            reg_lambda - amount of L2 regularisation
-            """
-            trainer.train_and_validate(
-                lambda: make_pipeline(
-                    model(device="gpu", scale_pos_weight=class_imbalance),
-                ),
-                MEAN_AGGREGATOR,
-                f"{model.__name__}_{receptor}_defaults",
-                {"ER": 0, "PR": 1}[receptor],
-            )
+            for gamma in (0, 1, 100):
+                for l1 in (0, 1, 100):
+                    for l2 in (0, 1, 100):
+                        trainer.train_and_validate(
+                            lambda: make_pipeline(
+                                model(
+                                    device="gpu",
+                                    gamma=gamma,
+                                    reg_alpha=l1,
+                                    reg_lambda=l2,
+                                    scale_pos_weight=class_imbalance,
+                                ),
+                            ),
+                            MEAN_AGGREGATOR,
+                            f"{model.__name__}_{receptor}_gamma={gamma}_l1={l1}_l2={l2}",
+                            {"ER": 0, "PR": 1}[receptor],
+                        )
 
 
 if __name__ == "__main__":
