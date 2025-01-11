d63a798c0355421b571022736ef589f8afef0696
qwertpi <qwertpi@users.noreply.github.com>
2025-01-11 17:15:40 +0000

    Give all activations and linear at end of blocks another go

--- experiments/edgeconv/econv.py
+++ experiments/edgeconv/econv.py
@@ -1,32 +1,49 @@
 # Based on https://github.com/engrodawood/HistBiases/blob/1aacf8ae86acfea5d29d55096a8af2b435ba99af/application/MLModels/SlideGraph%5E%7Binf%7D/model/gnn.py
 
+from abc import ABC, abstractmethod
 from functools import partial
 import sys
+from typing import Callable
 
 sys.path.insert(0, "..")
 
 import torch
 import torch_geometric.loader  # type: ignore
 import torch_geometric.nn  # type: ignore
-from tqdm.contrib.itertools import product
-
+from tqdm import tqdm
 from GNNModelTrainer import GNNModelTrainer  # type: ignore
 
 
-class Subnet(torch.nn.Module):
-    def __init__(self, in_dim: int, out_dim: int):
+class Subnet(torch.nn.Module, ABC):
+    @abstractmethod
+    def __init__(
+        self, in_dim: int, out_dim: int, act: Callable[[torch.Tensor], torch.Tensor]
+    ):
         super().__init__()
         self.lin = torch_geometric.nn.Linear(in_dim, out_dim)
         self.bn = torch.nn.BatchNorm1d(out_dim)
+        self.act = act
 
     def forward(self, x):
-        return self.bn(self.lin(x))
+        return self.act(self.bn(self.lin(x)))
+
+
+class InitialSubnet(Subnet):
+    def __init__(self):
+        super().__init__(1024, 1024, torch.nn.functional.gelu)
+
+
+class EdgeConvSubnet(Subnet):
+    def __init__(self):
+        super().__init__(2048, 1024, torch.nn.functional.elu)
 
 
 class Block(torch.nn.Module):
     def __init__(self, subblock: torch.nn.Module):
         super().__init__()
         self.subblock = subblock
+        self.lin = torch_geometric.nn.Linear(1024, 2)
+        self.bn = torch.nn.BatchNorm1d(2)
         self.dropout = torch.nn.Dropout(0.1)
 
     def forward(self, x, edge_index, batch):
@@ -34,6 +51,7 @@ class Block(torch.nn.Module):
             h = self.subblock(x, edge_index)
         else:
             h = self.subblock(x)
+        h = torch.nn.functional.elu(self.bn(self.lin(h)))
         h = torch_geometric.nn.pool.global_mean_pool(h, batch)
         return self.dropout(h)
 
@@ -42,14 +60,14 @@ class Model(torch.nn.Module):
     def __init__(self, num_initial_layers: int, num_middle_layers: int):
         super().__init__()
         subblocks = torch.nn.ModuleList(
-            [Subnet(1024, 1024) for _ in range(num_initial_layers)]
-            + [self._make_edgeconv() for _ in range(num_middle_layers)]
+            [InitialSubnet() for _ in range(num_initial_layers)]
+            + [
+                torch_geometric.nn.EdgeConv(EdgeConvSubnet(), aggr="max")
+                for _ in range(num_middle_layers)
+            ]
         )
         self.blocks = torch.nn.ModuleList([Block(subblock) for subblock in subblocks])
-        self.readout = torch.nn.Linear(1024, 2)
-
-    def _make_edgeconv(self):
-        return torch_geometric.nn.EdgeConv(Subnet(2048, 1024), aggr="max")
+        self.readout = torch.nn.Linear(2, 2)
 
     def forward(self, x, edge_index, batch):
         acc = torch.stack(
@@ -60,8 +78,10 @@ class Model(torch.nn.Module):
 
 if __name__ == "__main__":
     trainer = GNNModelTrainer()
-    trainer.train_and_validate(
-        partial(Model, 1, 2),
-        f"econv",
-        1e-2,
-    )
+
+    for start, middle in tqdm([(0, 1), (0, 2), (0, 3), (1, 1), (1, 2), (2, 1)]):
+        trainer.train_and_validate(
+            partial(Model, start, middle),
+            f"econv_act_s{start}_m{middle}",
+            1e-2,
+        )
