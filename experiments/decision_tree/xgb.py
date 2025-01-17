2c94c3be7893363e78c9c894c402ca7a20bbd44e
qwertpi <qwertpi@users.noreply.github.com>
2025-01-17 16:06:37 +0000

    Finalize xgboost hyperparams

--- experiments/decision_tree/xgb.py
+++ experiments/decision_tree/xgb.py
@@ -4,7 +4,6 @@ import sys
 sys.path.insert(0, "..")
 
 from sklearn.pipeline import make_pipeline  # type: ignore
-from tqdm.contrib.itertools import product
 from xgboost import XGBClassifier
 
 from PatchModelTrainer import PatchModelTrainer  # type: ignore
@@ -19,18 +18,12 @@ trainer = PatchModelTrainer()
 def train_model():
     er_class_imbalance = (NUM_EXAMPLES - NUM_POS["ER"]) / NUM_POS["ER"]
     pr_class_imbalance = (NUM_EXAMPLES - NUM_POS["PR"]) / NUM_POS["PR"]
-    gamma = 0
-    for l1, l2 in product([0, 1, 10, 100, 1000], [0, 1, 10, 100, 1000]):
-        model_func = partial(XGBClassifier, device="gpu", gamma=gamma, reg_alpha=l1, reg_lambda=l2)
-        trainer.train_and_validate(
-            lambda: make_pipeline(
-                model_func(scale_pos_weight=er_class_imbalance)
-            ),
-            lambda: make_pipeline(
-                model_func(scale_pos_weight=pr_class_imbalance)
-            ),
-            f"xgb_g{gamma}_l1{l1}_l2{l2}",
-        )
+    model_func = partial(XGBClassifier, device="gpu")
+    trainer.train_and_validate(
+        lambda: make_pipeline(model_func(scale_pos_weight=er_class_imbalance)),
+        lambda: make_pipeline(model_func(scale_pos_weight=pr_class_imbalance)),
+        "xgb",
+    )
 
 
 if __name__ == "__main__":
